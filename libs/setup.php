4490ffa3f53f2ddc1060aa07eb2805cbf4e6f4d9
frm666 <frm666@gmail.com>
2023-02-04 18:45:07 +0100

    version 02

--- libs/setup.php
+++ libs/setup.php
@@ -8,8 +8,18 @@ $config->set('modelsFolder', 'models/');
 $config->set('viewsFolder', 'views/');
 
 // Parámetros de conexión a la BD
-$config->set('dbhost', 'localhost');
-$config->set('dbname', 'test');
+// Parámetros de conexión a la BD
+//$config->set('dbhost', 'localhost');
+//$config->set('dbname', 'test');
+//$config->set('dbuser', 'root');
+//$config->set('dbpass', '');
+
+
+$config->set('dbhost', 'containers-us-west-179.railway.app:5845');
+$config->set('dbname', 'railway');
 $config->set('dbuser', 'root');
-$config->set('dbpass', '');
+$config->set('dbpass', 'MReAICGr5qPiqK6JHE9C');
+
+
+//mysql://root:MReAICGr5qPiqK6JHE9C@containers-us-west-179.railway.app:5845/railway
 ?>
